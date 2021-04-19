11647703131fff3c38e7c3bca8daa14a2443fee0
zakhar-kostyshyn <zakharkostyshyn@gmail.com>
2021-04-19 12:48:56 +0300

    стоворення простої форми і кнопки submit

--- exchange/src/main/java/com/exchange/example/controller/ExchangePageController.java
+++ exchange/src/main/java/com/exchange/example/controller/ExchangePageController.java
@@ -1,16 +1,32 @@
 package com.exchange.example.controller;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ExchangePageController {
 
     @GetMapping
-    public String page() {
+    public String mainPage(Model model) {
+        model.addAttribute("values", new Values());
         return "exchange_page";
     }
 
+    @PostMapping
+    public String formHandler(
+            @ModelAttribute("values") Values values
+    ) {
+        System.out.println("values = " + values);
+        return "redirect:/";
+    }
 
+}
 
+@Data
+class Values {
+    double gryvnas;
 }
